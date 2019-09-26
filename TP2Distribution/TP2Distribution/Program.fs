d0a863902928f88128e706a75eb9a8fd45566209
Samuel Dextraze <sam7945@hotmail.com>
2019-09-26 14:26:35 -0400

    #1

--- TP2Distribution/TP2Distribution/Program.fs
+++ TP2Distribution/TP2Distribution/Program.fs
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+//Méthode globale
+let charToInt c = int c - int '0'
+
+
+
+//x= nombre init,
+let Exercice1 x = 
+    let long = x.ToString().Length
+    let mutable y = 0
+    for i in x.ToString() do
+        let f:int = i|>charToInt
+        y <- y + (pown f long)
+    if y = x && x > 0 then "C'est un chiffre narcissique"
+    else "Ce n'est pas un chiffre narcissique"
+      
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.WriteLine("Exercice #1 : "+Exercice1 0)
+    Console.ReadKey()|>ignore;
     0 // return an integer exit code
